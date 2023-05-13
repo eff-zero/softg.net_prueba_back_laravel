a4c2419d60e3a0ba8ce39b944056b58f8b280ed5
Jesus Hernandez <jehersi988@gmail.com>
2023-05-13 09:21:18 -0500

    DriverModule: modelo

--- app/Modules/DriverModule/Driver.php
+++ app/Modules/DriverModule/Driver.php
@@ -2,12 +2,58 @@
 
 namespace App\Modules\DriverModule;
 
+use App\Helpers\RestActions;
+use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Driver extends Model
 {
-    use HasFactory;
+    use HasFactory, RestActions, SoftDeletes;
 
     protected $table = 'users';
+    protected $guarded = [];
+    protected $hidden = [
+        'active',
+        'deleted_at',
+        'remember_token',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function saveDriver(array $data)
+    {
+        return $this::create($data);
+    }
+
+    public function getDriver(int $id)
+    {
+        return $this::whereNotNull('ssn')->find($id);
+    }
+
+    public function getDrivers()
+    {
+        return $this::whereNotNull('ssn')->get();
+    }
+
+    public function deleteDriver(int $id)
+    {
+        $driver = $this::whereNotNull('ssn')->find($id);
+        if (!$driver) {
+            return false;
+        }
+        $driver->delete();
+        return $driver;
+    }
+
+    public function updateDriver(array $data, int $id)
+    {
+        $driver = $this::whereNotNull('ssn')->find($id);
+        if (!$driver) {
+            return false;
+        }
+        $driver->update($data);
+        return $driver;
+    }
 }
